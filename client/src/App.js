807ea01c6ac93e25c36d55b217a962a95ba97612
Debbie Fan <debbiehmfan@outlook.com>
2023-02-22 19:28:52 -0500

    Connect React Client to the Express API

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Router from './routes/router'
 import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className='app'>
-      <main> 
-        <Navbar />
-        <Router/>
-      </main>
-      <footer>Last updated Feb 2023</footer>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+
+  callAPI() {
+    fetch("http://localhost:9000/testAPI")
+      .then(res => res.text())
+      .then(res => this.setState({ apiResponse: res }));
+  }
+
+  componentDidMount() {
+    this.callAPI();
+  }
+
+  render() {
+    return (
+      <div className='app'>
+        <main> 
+          <Navbar />
+          <Router/>
+          <p className='App-intro'>{this.state.apiResponse}</p>
+        </main>
+        <footer>Last updated Feb 2023</footer>
+      </div>
+    );
+  }
 }
 
 export default App;
